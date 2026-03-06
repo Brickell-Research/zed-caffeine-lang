2b58d89a353f8be1c1bdb63a5173284898ba9868
robertDurst <me@robdurst.com>
2026-03-06 07:22:18 -0700

    simplify

--- languages/caffeine/indents.scm
+++
@@ -1,8 +0,0 @@
-(blueprint_block) @indent
-(expectation_block) @indent
-(item) @indent
-(requires_block) @indent
-(provides_block) @indent
-(_ "{" "}" @end) @indent
-(_ "[" "]" @end) @indent
-(_ "(" ")" @end) @indent
